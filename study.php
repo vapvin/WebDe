41c8e726cfa477a8299956ea2c2c2d004c25f35d
vapvin <tpehdj12@daum.net>
2019-11-30 19:19:31 +0900

    Push

--- study.php
+++ study.php
@@ -1,29 +1,30 @@
-//배송지역 체크 시 이벤트
-billingShppingOption.on('change', function(){
-billing_shipping_option($(this).val(), '');
-$('body').trigger('update_checkout');
-});
-shippingShppingOption.on('change', function(){
-shipping_shipping_option($(this).val(), '');
-$('body').trigger('update_checkout');
-});
 
-/**Select 옵션**/
-function set_original_select($select) {
-if ($select.data("originalHTML") == undefined) {
-$select.data("originalHTML", $select.html());
-} // If it's already there, don't re-set it
+add_action('template_redirect', 'redirect_guest_checkout_to_login');
+
+//체크아웃 에러메세지
+add_filter( 'woocommerce_form_field', 'checkout_fields_in_label_error', 10, 4 );
+function checkout_fields_in_label_error( $field, $key, $args, $value ) {
+	if ( strpos( $field, '</p>' ) !== false && $args['required'] ) {
+		$error = '<span class="error" style="display:none">';
+		$error .= sprintf( __( '%s is a required field.', 'woocommerce' ), $args['label'] );
+		$error .= '</span>';
+		$field = substr_replace( $field, $error, strpos( $field, '</p>' ), 0);
+	}
+	return $field;
 }
 
-function remove_options($select, $options) {
-set_original_select($select);
-$options.remove();
+add_action( 'wp_footer', 'misha_checkout_js' );
+function misha_checkout_js(){
+	// we need it only on our checkout page
+	if( !is_checkout() ) return;
+	?>
+	<script>
+	(function($){
+		$(document.body).on('checkout_error', function(){
+			jQuery( 'html, body' ).stop();
+			jQuery( 'html, body' ).animate({scrollTop:0}, 600);
+		});
+	})(jQuery);
+	</script>
+	<?php
 }
-
-function restore_options($select) {
-var ogHTML = $select.data("originalHTML");
-if (ogHTML != undefined) {
-$select.html(ogHTML);
-}
-}
-/**Select 옵션**/
